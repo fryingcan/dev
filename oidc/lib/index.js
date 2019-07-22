329774da9a77c2ae40fb54d45f74da75875fbefb
fryingcan <aram.peres+fryingcan@gmail.com>
2019-07-22 10:31:40 -0400

    move proxy config

--- oidc/lib/index.js
+++ oidc/lib/index.js
@@ -7,11 +7,11 @@ console.log("Starting provider.");
 
 const oidc = new Provider('https://openid.momoperes.ca');
 
-// behind HTTPS proxy
-app.enable('trust proxy');
-oidc.proxy = true;
-
 oidc.initialize().then(function () {
+    // behind HTTPS proxy
+    app.enable('trust proxy');
+    oidc.proxy = true;
+
     app.use('/', oidc.callback);
     app.listen(3000, "0.0.0.0");
 });
