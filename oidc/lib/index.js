5bc80420c67b61d7b6b54177a2765443abff0e4a
fryingcan <aram.peres+fryingcan@gmail.com>
2019-07-22 09:57:13 -0400

    Start work on oidc

---
+++ oidc/lib/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const Provider = require('oidc-provider');
+
+const app = express();
+
+console.log("Starting provider.");
+
+const oidc = new Provider('https://openid.momoperes.ca');
+oidc.initialize().then(function () {
+    app.use('/', oidc.callback);
+    app.listen(3000, "0.0.0.0");
+});
