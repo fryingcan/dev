e4c6fa2ed5bc991c4f05bb9f4337132bf94aa018
fryingcan <aram.peres+fryingcan@gmail.com>
2019-07-22 10:26:33 -0400

    trust proxy

--- oidc/lib/index.js
+++ oidc/lib/index.js
@@ -6,6 +6,11 @@ const app = express();
 console.log("Starting provider.");
 
 const oidc = new Provider('https://openid.momoperes.ca');
+
+// behind HTTPS proxy
+app.enable('trust proxy');
+oidc.proxy = true;
+
 oidc.initialize().then(function () {
     app.use('/', oidc.callback);
     app.listen(3000, "0.0.0.0");
